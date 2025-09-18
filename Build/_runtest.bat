da37bf9cc1bfacc9ed4a9bbe0993822ae4101b8f
Fenex <31255152+thumpnail@users.noreply.github.com>
2025-09-19 01:02:40 +0200

    Add build outputs and update workspace settings
    Added new and updated DLLs, PDBs, and JSON files for ChatApp, its services, and dependencies in the Build directory. Updated .idea workspace.xml to adjust run configurations and task durations. Also included new test and report artifacts.

--- Build/_runtest.bat
+++ Build/_runtest.bat
@@ -3,9 +3,9 @@
 echo Parameters: [lite, memory, mock]
 
 start "" ".\ChatHistoryService.exe"
-start "" ".\ChatDatabaseService.exe" lite
+start "" ".\ChatDatabaseService.exe" memory
 start "" ".\ChatMessagingService.exe"
-start "" ".\ChatApp.Server.exe" lite
+start "" ".\ChatApp.Server.exe" memory
 
 echo Parameters: [lite, memory]
 .\Chat.Tests.exe memory
